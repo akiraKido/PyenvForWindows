a9f297b926f93d194f3b386bf2d535e90e347513
Akira Kido <achts.y@gmail.com>
2017-05-03 14:49:57 +0900

    バージョンの切り替えを行えるよう変更

--- PyenvForWindows/MainForm.Designer.cs
+++ PyenvForWindows/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.InstalledPython_ListBox = new System.Windows.Forms.ListBox();
+            this.SwithTo_Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // InstalledPython_ListBox
@@ -40,11 +41,22 @@
             this.InstalledPython_ListBox.Size = new System.Drawing.Size(259, 208);
             this.InstalledPython_ListBox.TabIndex = 0;
             // 
+            // SwithTo_Button
+            // 
+            this.SwithTo_Button.Location = new System.Drawing.Point(13, 227);
+            this.SwithTo_Button.Name = "SwithTo_Button";
+            this.SwithTo_Button.Size = new System.Drawing.Size(75, 23);
+            this.SwithTo_Button.TabIndex = 1;
+            this.SwithTo_Button.Text = "Switch";
+            this.SwithTo_Button.UseVisualStyleBackColor = true;
+            this.SwithTo_Button.Click += new System.EventHandler(this.SwithTo_Button_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.SwithTo_Button);
             this.Controls.Add(this.InstalledPython_ListBox);
             this.Name = "MainForm";
             this.Text = "MainForm";
@@ -55,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.ListBox InstalledPython_ListBox;
+        private System.Windows.Forms.Button SwithTo_Button;
     }
 }
 
--- PyenvForWindows/MainForm.cs
+++ PyenvForWindows/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
@@ -7,8 +8,7 @@ namespace PyenvForWindows
 {
     public partial class MainForm : Form
     {
-        private PythonDirectoryInfo[] installedPythonInfos;
-
+        private DirectoryInfo pythonRootDirectory = new DirectoryInfo(@"C:\Python");
 
         public MainForm()
         {
@@ -19,33 +19,41 @@ namespace PyenvForWindows
 
         private void DisplayInstalledPythons()
         {
-            var installedPythons = Directory.GetDirectories(@"C:\Python");
-
-            var infos = installedPythons.Select(
-                installedPython =>
-                new PythonDirectoryInfo(
-                    Path.GetFileName(installedPython),
-                    installedPython)
-                ).ToArray();
-
-            foreach (var info in infos)
+            foreach (var info in pythonRootDirectory.GetDirectories())
             {
                 InstalledPython_ListBox.Items.Add(info.Name);
             }
-
-            installedPythonInfos = infos;
         }
 
-        private class PythonDirectoryInfo
+        private void SwithTo_Button_Click(object sender, EventArgs e)
         {
-            internal string Name { get; }
-            internal string Path { get; }
-
-            internal PythonDirectoryInfo(string name, string path)
+            string selectedVersion;
+            try
             {
-                Name = name;
-                Path = path;
+                selectedVersion = InstalledPython_ListBox.SelectedItem.ToString();
+            } catch (Exception) { return; }
+
+            string selectedPythonPath = pythonRootDirectory.GetDirectories().First(info => info.Name == selectedVersion).FullName;
+            
+            // 環境変数の取得
+            string pathString = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+            if (pathString == null)
+            {
+                Console.WriteLine("Could not achieve path.");
+                return;
             }
+
+            // 登録されている Path の一覧を取得
+            var paths = pathString.Split(';');
+            // 現在登録されている Python を環境変数から外す
+            var newPaths = paths.Where(path => !path.Contains(pythonRootDirectory.FullName)).ToList();
+
+            // 指定されたバージョンの Python を環境変数に登録
+            newPaths.Add(selectedPythonPath);
+            newPaths.Add(Path.Combine(selectedPythonPath, "Scripts"));
+
+            // 環境変数を更新
+            Environment.SetEnvironmentVariable("Path", string.Join(";", newPaths), EnvironmentVariableTarget.Machine);
         }
     }
 }
